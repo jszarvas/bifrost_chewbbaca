438ca024202fed924b8444615368f5e34794e792
Judit Szarvas <szarvas.judit@gmail.com>
2026-08-13 09:57:02 +0200

    Bump version: 2.1.2 → 2.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="bifrost_chewbbaca",
-    version="2.1.2",
+    version="2.1.3",
     url="https://github.com/ssi-dk/bifrost_chewbbaca",
     author="Kim Ng",
     author_email="kimn@ssi.dk",
